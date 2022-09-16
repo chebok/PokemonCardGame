70c3aab0f0a588664532a03d74a1af2f028e49f3
chebok <vovakuzmin@gmail.com>
2022-09-16 19:10:31 +0300

    remove server to server dir

--- server/authController.js
+++ server/authController.js
@@ -1,28 +1,27 @@
-const User = require('./models/User');
-const Role = require('./models/Role');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
+const Role = require('./models/Role');
+const User = require('./models/User');
 const { secret } = require('./config');
 
 const generateAccessToken = (id, roles) => {
   const payload = {
     id,
-    roles
-  }
-  return jwt.sign(payload, secret, {expiresIn: '24h'});
-}
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: '24h' });
+};
 
 class AuthController {
-
   async createUser(req, res) {
     try {
       res.render('create', {
         title: 'Registration',
-        
+
       });
     } catch (error) {
-        
+
     }
   }
 
@@ -32,7 +31,7 @@ class AuthController {
         title: 'Login',
       });
     } catch (error) {
-        
+
     }
   }
 
@@ -40,28 +39,28 @@ class AuthController {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json({message: 'Ошибка при регистрации', errors });
+        return res.status(400).json({ message: 'Ошибка при регистрации', errors });
       }
       const { username, password } = req.body;
-      const candidate = await User.findOne({username});
+      const candidate = await User.findOne({ username });
       if (candidate) {
-        return res.status(400).json({message: 'Такой пользователь уже существует'});
+        return res.status(400).json({ message: 'Такой пользователь уже существует' });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
-      const userRole = await Role.findOne({value: "USER"});
+      const userRole = await Role.findOne({ value: 'USER' });
       const user = new User({ username, password: hashPassword, roles: [userRole.value] });
       await user.save();
-      return res.json({message: 'Пользователь успешно зарегистрирован'});
+      return res.json({ message: 'Пользователь успешно зарегистрирован' });
     } catch (error) {
       console.log(error);
-      res.status(400).json({message: 'Registration error'});
+      res.status(400).json({ message: 'Registration error' });
     }
   }
 
   async login(req, res) {
     try {
       const { username, password } = req.body;
-      const user = await User.findOne({username});
+      const user = await User.findOne({ username });
       if (!user) {
         return res.status(400).json({ message: `Пользователь ${username} не найден` });
       }
@@ -70,10 +69,10 @@ class AuthController {
         return res.status(400).json({ message: 'Введен неверный пароль' });
       }
       const token = generateAccessToken(user._id, user.roles);
-      return res.json({token});
+      return res.json({ token });
     } catch (error) {
       console.log(error);
-      res.status(400).json({message: 'Login error'});
+      res.status(400).json({ message: 'Login error' });
     }
   }
 
@@ -90,7 +89,7 @@ class AuthController {
         users,
       });
     } catch (error) {
-        
+
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const path = require('node:path');
 const authRouter = require('./authRouter');
 require('dotenv').config();
 
@@ -8,37 +9,39 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', authRouter);
+
 app.get('/', (req, res) => {
   try {
     res.render('index', {
       title: 'Pokemons',
     });
   } catch (error) {
-      
+
   }
-})
-app.use('/', authRouter);
+});
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views','views');
+app.set('views', 'views');
 
 const start = async () => {
-    try {
-      await mongoose.connect('mongodb+srv://Chebok:202Seldon@cluster0.h58165t.mongodb.net/mongo-node-app', {
-        useNewUrlParser: true,
-      });
-      app.listen(PORT, () => {
-          console.log(`Server has been started... on port ${PORT}`);
-      })
-    } catch (e) {
-      console.log(e)
-    }
+  try {
+    await mongoose.connect('mongodb+srv://Chebok:202Seldon@cluster0.h58165t.mongodb.net/mongo-node-app', {
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server has been started... on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  
-  start();
+};
+
+start();
